953578d5843dd0a8f31f408cd9d56188a6732c94
rajatgagat81 <54751975+rajatgagat81@users.noreply.github.com>
2021-12-07 21:39:51 +0530

    Add files via upload
    Assignment

---
+++ relevel-trello-clone/trello-clone-template-main/README.md
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>releve trello</title>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
---
+++ relevel-trello-clone/trello-clone-template-main/src/components/TrelloBoard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+class TrelloBoard extends React.Component {
+
+    render() {
+        return (
+            /*Can start Coding from Here*/
+            <div></div>
+        )
+    }
+}
+
+export default TrelloBoard;
